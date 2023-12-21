bc0c1d1e8d2110d4a119c7d1b7f9875a2db20ee3
Tatiana <tatianibassani@gmail.com>
2023-12-21 16:58:27 +0000

    add urls_new in views, Add a GET Route to Show the Form

--- express_server.js
+++ express_server.js
@@ -30,7 +30,23 @@ app.get("/urls.json", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req, res) => {
+    console.log('test');
+    res.render("urls_new");
+  });
+
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase };
     res.render("urls_show", templateVars);
   });
+
+  app.use(express.urlencoded({ extended: true }));
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
+
+  function generateRandomString() {
+
+  }
